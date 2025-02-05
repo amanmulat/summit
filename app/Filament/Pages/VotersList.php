7d114ecd1a5f5b4f21b423db29c2f5ffaefb8b6d
adane <adaneworis@gmail.com>
2025-02-05 12:54:31 +0300

    paginate voter list test-5

--- app/Filament/Pages/VotersList.php
+++ app/Filament/Pages/VotersList.php
@@ -22,7 +22,7 @@ class VotersList extends Page implements HasTable
     {
         return $table
             ->paginated([10, 25, 50, 100, 200, 'all'])
-            ->defaultPaginationPageOption(25)
+            ->defaultPaginationPageOption(200)
             ->query(Vote::query())
             ->columns([
                 TextColumn::make('user.email')
